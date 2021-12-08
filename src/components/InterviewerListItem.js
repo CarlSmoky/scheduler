58f17d1ebb9e674626036732f4acecad6e43f2f1
tsumitakaoru <k.tsumita01@gmail.com>
2021-12-08 16:48:46 -0500

    Refactor InterviewList Components

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -10,13 +10,13 @@ export default function InterviewerListItem(props) {
   );
 
   return (
-      <li className={interviewListItemClass} onClick={() => props.setInterviewer(props.id)}>
+      <li className={interviewListItemClass} onClick={props.setInterviewer}>
         <img
           className="interviewers__item-image"
           src={props.avatar}
           alt={props.name}
         />
-        {props.selected && <p>{props.name}</p>}
+        {props.selected && props.name}
       </li>
   );
 }
