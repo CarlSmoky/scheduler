b33614a7959a2327bd82ee384b555add370cf8f5
tsumitakaoru <k.tsumita01@gmail.com>
2021-12-09 11:44:57 -0500

    Refactor

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -4,17 +4,20 @@ import InterviewerListItem from "./InterviewerListItem";
 
 export default function InterviewerList(props) {
 
+  // console.log(props.interviewer);
 
-  const interviewerListItems = props.interviewers.map(interviewer =>
-    <InterviewerListItem
-      key={interviewer.id}
-      // id={interviewer.id}
-      name={interviewer.name}
-      avatar={interviewer.avatar}
-      selected={interviewer.id === props.value}
-      setInterviewer={(event) => props.onChange(interviewer.id)}
-    />
-  )
+  // note: I changed const interviewers = ... to const interviewerListItems = ...
+  const interviewerListItems = props.interviewers.map((interviewer) => {
+    return (
+      <InterviewerListItem
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === props.interviewer}
+        setInterviewer={() => props.onChange(interviewer.id)}
+      />
+    );
+  });
 
   return (
     <section className="interviewers">
--- stories/index.js
+++ stories/index.js
@@ -154,8 +154,8 @@ storiesOf("Appointment", module)
   .add("Confirm", () => <Confirm onConfirm={action("onConfirm")} onCancel={action("onCancel")}/>)
   .add("Status", () => <Status message="Deleteing" />)
   .add("Error", () => <Error message="Could not delete appointment." onClose={action("onClose")}/>)
-  // .add("Edit", () => <Form student="Kaoru" interviewer={0} interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")}/>)
-  // .add("Create", () => <Create interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")}/>)
+  .add("Edit", () => <Form student="Kaoru" interviewer={0} interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")}/>)
+  .add("Create", () => <Form interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")}/>)
 
 
   
