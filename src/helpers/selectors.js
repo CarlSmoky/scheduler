515e1f250140710f5cf4c619dfa37e736080aec3
tsumitakaoru <k.tsumita01@gmail.com>
2021-12-11 09:14:50 -0500

    Add helper

---
+++ src/helpers/selectors.js
@@ -0,0 +1,20 @@
+export function getAppointmentsForDay(state, day) {
+  const days = state.days;
+  const appointments = Object.values(state.appointments || {} );
+
+  const selectedDayArray = days.filter(aDay => aDay.name === day);
+  if (selectedDayArray.length === 0) {
+    return [];
+  }
+
+  const selectedDaysAppointments = selectedDayArray[0].appointments;
+  let result = [];
+  for (let appId of selectedDaysAppointments) {
+    for (let appt of appointments) {
+      if (appId === appt.id) {
+        result.push(appt);
+      }
+    }
+  }
+  return result;
+}
