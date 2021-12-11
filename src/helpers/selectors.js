a924f484b62c289f401c21c4d129a5613f8e18b7
tsumitakaoru <k.tsumita01@gmail.com>
2021-12-11 11:44:32 -0500

    change export default

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,4 +1,4 @@
-export function getAppointmentsForDay(state, day) {
+export default function getAppointmentsForDay(state, day) {
   const days = state.days;
   const appointments = Object.values(state.appointments || {} );
 
