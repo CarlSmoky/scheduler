f21427eab6911fbb0c9a129a86992bba5d13f50f
tsumitakaoru <k.tsumita01@gmail.com>
2021-12-17 14:18:30 -0500

    Delete comments

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,45 +2,19 @@ import React from "react";
 import "components/Button.scss";
 import classNames from "classnames";
 
-// export default function Button(props) {
-//    return <></>;
-// }
+export default function Button(props) {
+  const buttonClass = classNames("button", {
+    "button--confirm": props.confirm,
+    "button--danger": props.danger
+  });
 
-// export default function Button(props) {
-//    let buttonClass = "button";
- 
-//    if (props.confirm) {
-//      buttonClass += " button--confirm";
-//    }
- 
-//    if (props.danger) {
-//      buttonClass += " button--danger";
-//    }
- 
-//    return (
-//      <button
-//        className={buttonClass}
-//        onClick={props.onClick}
-//        disabled={props.disabled}
-//      >
-//        {props.children}
-//      </button>
-//    );
-//  }
-
- export default function Button(props) {
-   const buttonClass = classNames("button", {
-     "button--confirm": props.confirm,
-     "button--danger": props.danger
-   });
- 
-   return (
-     <button
-       className={buttonClass}
-       onClick={props.onClick}
-       disabled={props.disabled}
-     >
-       {props.children}
-     </button>
-   );
- }
+  return (
+    <button
+      className={buttonClass}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
+}
